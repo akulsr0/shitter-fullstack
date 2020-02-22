11caddc0b33bf346681eb0596d4ee11bff2ea0fc
akulsrivastava <akulsr0@gmail.com>
2020-02-22 20:43:59 +0530

    friends view

--- routes/friends.js
+++ routes/friends.js
@@ -5,6 +5,18 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
 
+router.get('/', async (req, res) => {
+  try {
+    const tokenCookie = req.headers.cookie;
+    const token = tokenCookie.split('=')[1];
+    const decoded = jwt.verify(token, config.get('JWT_SECRET'));
+    const user = await User.findById(decoded.id).select('-password');
+    res.render('friends', { user });
+  } catch (error) {
+    res.json(error);
+  }
+});
+
 router.post('/:uid/remove', async (req, res) => {
   try {
     const { uid } = req.params;
